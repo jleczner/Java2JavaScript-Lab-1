9cab78ec6791c2efc0d17f2fb87f1fe54f462b36
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-09-30 16:57:18 -0400

    solved first problem

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 
 var display = document.getElementById("display");
 
-function yourFunctionName (){
-  display.innerHTML = "hello";
+function oneToTen () {
+	for (var i = 1; i < 11; i++) {
+  		display.innerHTML += i + "<br>";
+
+  	}
 }
+
+oneToTen();
